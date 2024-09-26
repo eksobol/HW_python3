7eafbb424746dae0bf52f1f8ef4d37cf4b46c406
Ekaterina Soboleva <ek.a.soboleva@ya.ru>
2024-09-26 23:59:32 +0300

    +upd_eng

--- test_search.py
+++ test_search.py
@@ -2,12 +2,12 @@ from selene import browser, be, have
 
 
 def test_success(browser_settings):
-    browser.open('https://google.com')
+    browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 def test_fail(browser_settings):
     text = "вдлыомдшщгу9гповмлдтотол"
-    browser.open('https://google.com')
+    browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type(text).press_enter()
-    browser.element('div.card-section').should(have.text(f'По запросу {text} ничего не найдено.'))
+    browser.element('div.card-section').should(have.text(f'Your search - {text} - did not match any documents.'))
